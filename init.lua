69917300b7c228664a0585dea1f79f0b4f0729e2
JiangNight <wan1298178043@gmail.com>
2025-03-06 09:45:14 +0800

    add code highlighting.

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 -- INFO: set lazyrepo and local path
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+	local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ 'Failed to clone lazy.nvim\n', 'ErrorMsg' },
+			{ out,                           'WarningMsg' },
+			{ '\nPress any key to exit...' },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -19,4 +19,4 @@ vim.opt.rtp:prepend(lazypath)
 require 'config.options' -- INFO: Basic Setting options
 require 'config.keymaps' -- INFO: Basic Keymaps
 require 'config.command' -- INFO: Basic AutoCommands
-require 'lazy_setup'   -- INFO: Loading lazy plugins
+require 'lazy_setup'     -- INFO: Loading lazy plugins
---
+++ lua/plugins/lang/treesitter.lua
@@ -0,0 +1,50 @@
+--INFO: treesitter ensure_installed
+local ensure_installed = {
+	'lua',
+	'vim',
+	'c',
+	'json',
+}
+return { -- 高亮显示，编辑和导航代码
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+	main = 'nvim-treesitter.config', -- 设置用于opts的主模块
+	-- [[配置 Treesitter]]参见 ':help nvim-treesotter'
+	config = function()
+		require('nvim-treesitter.configs').setup {
+			modules = {},
+			ensure_installed = ensure_installed,
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
+			-- Automatically install missing parsers when entering buffer
+			auto_install = true,
+			-- List of parsers to ignore installing (for "all")
+			ignore_install = { 'tmux' },
+			-- 支持语言
+			-- 启用代码高亮
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			-- 启用增量选择
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = '<CR>',
+					node_incremental = '<CR>',
+					node_decremental = '<BS>',
+					scope_incremental = '<TAB>',
+				},
+			},
+			-- 启用基于Treesitter的代码格式化(=)
+			indent = {
+				enable = false,
+			},
+		}
+		-- 开启代码折叠
+		vim.opt.foldmethod = 'expr'
+		vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+		-- 默认不折叠
+		-- vim.wo.foldlevel = 99
+	end,
+}
