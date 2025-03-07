257308d5233bae12d46dafe7b91583ef0a893a27
JiangNight <wan1298178043@gmail.com>
2025-03-07 23:38:16 +0800

    snacks: picker format

--- lua/custom/Snacks/picker.lua
+++ lua/custom/Snacks/picker.lua
@@ -20,14 +20,21 @@ M.options = {
 	},
 }
 M.keys = {
-	{
-		'<leader>ft',
-		function()
-			Snacks.picker.colorschemes()
-		end,
-		desc = 'Colorschemes',
-	},
-	{
+	{ '<leader>ft', function() Snacks.picker.colorschemes() end,                          desc = 'Colorschemes' },
+	{ '<leader>fn', function() Snacks.picker.smart { cwd = vim.fn.stdpath 'config' } end, desc = 'Smart Find Nvim Config' },
+	{'<leader>fh', function() Snacks.picker.help() end, desc = 'Help Pages' },
+	{ '<leader>fk', function() Snacks.picker.keymaps() end, desc = 'keymaps' },
+	{ '<leader>ff', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
+	{ '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+	{ '<leader>fl', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
+	{ '<leader>fg', function() Snacks.picker.grep() end, desc = 'Grep' },
+	{ '<leader>fw', function() Snacks.picker.grep_word() end, desc = 'Visual selection or word', mode = { 'n', 'x' } },
+	{ '<leader>fB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
+	{ '<leader>f.', function() Snacks.picker.resume() end, desc = 'Recent' },
+	{ '<leader>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
+	{ '<leader>fD', function() Snacks.picker.diagnostics() end, desc = 'Diagnostics' },
+	{ '<leader>fd', function() Snacks.picker.diagnostics_buffer() end, desc = 'Buffer Diagnostics' },
+    {
 		'<leader>fc',
 		function()
 			Snacks.picker.cliphist {
@@ -53,91 +60,5 @@ M.keys = {
 		end,
 		desc = 'Cliphist',
 	},
-	{
-		'<leader>fn',
-		function()
-			Snacks.picker.smart { cwd = vim.fn.stdpath 'config' }
-		end,
-		desc = 'Smart Find Nvim Config',
-	},
-	{
-		'<leader>fh',
-		function()
-			Snacks.picker.help()
-		end,
-		desc = 'Help Pages'
-	},
-	{
-		'<leader>fk',
-		function()
-			Snacks.picker.keymaps()
-		end,
-		desc = 'keymaps'
-	},
-	{
-		'<leader>ff',
-		function()
-			Snacks.picker.smart()
-		end,
-		desc = 'Smart Find Files',
-	},
-	{
-		'<leader>fb',
-		function()
-			Snacks.picker.buffers()
-		end,
-		desc = 'Buffers',
-	},
-	{
-		'<leader>fl',
-		function()
-			Snacks.picker.lines()
-		end,
-		desc = 'Buffer Lines',
-	},
-	{
-		'<leader>fg',
-		function()
-			Snacks.picker.grep()
-		end,
-		desc = 'Grep',
-	},
-	{
-		'<leader>fw',
-		function()
-			Snacks.picker.grep_word()
-		end,
-		desc = 'Visual selection or word',
-		mode = { 'n', 'x' },
-	},
-	{
-		'<leader>fB',
-		function()
-			Snacks.picker.grep_buffers()
-		end,
-		desc = 'Grep Open Buffers',
-	},
-	{
-		'<leader>f.',
-		function()
-			Snacks.picker.resume()
-		end,
-		desc = 'Recent',
-	},
-	{
-		'<leader>fr',
-		function()
-			Snacks.picker.recent()
-		end,
-		desc = 'Recent',
-	},
-	{
-		'<leader>fD',
-		function()
-			Snacks.picker.diagnostics()
-		end,
-		desc = 'Diagnostics',
-	},
 }
 return M
-
