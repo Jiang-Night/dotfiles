98cd7e4461a1d697a363324123cb33df6a425b15
JiangNight <wan1298178043@gmail.com>
2025-03-07 23:49:46 +0800

    snacks: Picker shortcut key supplement

--- lua/custom/Snacks/picker.lua
+++ lua/custom/Snacks/picker.lua
@@ -22,19 +22,76 @@ M.options = {
 M.keys = {
 	{ '<leader>ft', function() Snacks.picker.colorschemes() end,                          desc = 'Colorschemes' },
 	{ '<leader>fn', function() Snacks.picker.smart { cwd = vim.fn.stdpath 'config' } end, desc = 'Smart Find Nvim Config' },
-	{'<leader>fh', function() Snacks.picker.help() end, desc = 'Help Pages' },
-	{ '<leader>fk', function() Snacks.picker.keymaps() end, desc = 'keymaps' },
-	{ '<leader>ff', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
-	{ '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
-	{ '<leader>fl', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
-	{ '<leader>fg', function() Snacks.picker.grep() end, desc = 'Grep' },
-	{ '<leader>fw', function() Snacks.picker.grep_word() end, desc = 'Visual selection or word', mode = { 'n', 'x' } },
-	{ '<leader>fB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
-	{ '<leader>f.', function() Snacks.picker.resume() end, desc = 'Recent' },
-	{ '<leader>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
-	{ '<leader>fD', function() Snacks.picker.diagnostics() end, desc = 'Diagnostics' },
-	{ '<leader>fd', function() Snacks.picker.diagnostics_buffer() end, desc = 'Buffer Diagnostics' },
-    {
+	{ '<leader>fh', function() Snacks.picker.help() end,                                  desc = 'Help Pages' },
+	{ '<leader>fk', function() Snacks.picker.keymaps() end,                               desc = 'keymaps' },
+	{ '<leader>ff', function() Snacks.picker.smart() end,                                 desc = 'Smart Find Files' },
+	{ '<leader>fb', function() Snacks.picker.buffers() end,                               desc = 'Buffers' },
+	{ '<leader>fl', function() Snacks.picker.lines() end,                                 desc = 'Buffer Lines' },
+	{ '<leader>fg', function() Snacks.picker.grep() end,                                  desc = 'Grep' },
+	{ '<leader>fw', function() Snacks.picker.grep_word() end,                             desc = 'Visual selection or word', mode = { 'n', 'x' } },
+	{ '<leader>fB', function() Snacks.picker.grep_buffers() end,                          desc = 'Grep Open Buffers' },
+	{ '<leader>f.', function() Snacks.picker.resume() end,                                desc = 'Recent' },
+	{ '<leader>fr', function() Snacks.picker.recent() end,                                desc = 'Recent' },
+	{ '<leader>fD', function() Snacks.picker.diagnostics() end,                           desc = 'Diagnostics' },
+	{ '<leader>fd', function() Snacks.picker.diagnostics_buffer() end,                    desc = 'Buffer Diagnostics' },
+	{ '<leader>fu', function() Snacks.picker.undo() end,                                  desc = '[Find] [U]ndo' },
+	{ '<leader>fu', function() Snacks.picker.undo() end,                                  desc = '[Find] [U]ndo' },
+	{
+		'<leader>fo',
+		function()
+			Snacks.picker.noice {
+				layout = {
+					layout = {
+						box = 'vertical',
+						backdrop = false,
+						row = -1,
+						width = 0,
+						height = 0.4,
+						border = 'top',
+						title = ' {title} {live} {flags}',
+						title_pos = 'left',
+						{ win = 'input', height = 1, border = 'bottom' },
+						{
+							box = 'horizontal',
+							{ win = 'list',    border = 'none' },
+							{ win = 'preview', title = '{preview}', width = 0.6, border = 'left' },
+						},
+					},
+				},
+			}
+		end,
+		desc = '[Find] [Old] [N]otifications',
+	},
+	{
+		'<leader>fa',
+		function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			local clients = vim.lsp.get_clients { bufnr = bufnr }
+			local function has_lsp_symbols()
+				for _, client in pairs(clients) do
+					if client.server_capabilities.documentSymbolProvider then
+						return true
+					end
+				end
+				return false
+			end
+
+			local picker_opts = {
+				layout = 'right',
+				tree = true,
+				on_show = function()
+					vim.cmd.stopinsert()
+				end,
+			}
+			if has_lsp_symbols() then
+				Snacks.picker.lsp_symbols(picker_opts)
+			else
+				Snacks.picker.treesitter()
+			end
+		end,
+		desc = 'LSP Symbols'
+	},
+	{
 		'<leader>fc',
 		function()
 			Snacks.picker.cliphist {
