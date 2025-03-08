3e742bbeb6f4724f733acad84fe4b404ea3883b5
JiangNight <wan1298178043@gmail.com>
2025-03-09 00:05:19 +0800

    fix autopairs

--- lua/plugins/utils/autopairs.lua
+++ lua/plugins/utils/autopairs.lua
@@ -1,7 +1,7 @@
 -- 自动补全括号等
 return {
     'windwp/nvim-autopairs',
-    event = 'InserEnter',
+    event = 'InsertEnter',
     config = function()
         require('nvim-autopairs').setup { map_cr = true }
     end,
